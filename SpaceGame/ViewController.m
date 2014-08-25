6d53a9c48975450ede2bb2983e68b78173207b87
An Qi <guy_asdf@hotmail.com>
2014-08-25 15:15:34 -0400

    has banner ads, although messes up game screen. don’t need to have here

--- SpaceGame/ViewController.m
+++ SpaceGame/ViewController.m
@@ -9,12 +9,20 @@
 #import "ViewController.h"
 #import "MyScene.h"
 
+@interface ViewController () <ADBannerViewDelegate>
+@property (nonatomic, strong) ADBannerView *BannerAd;
+@end
+
 @implementation ViewController
 
 - (void)viewWillLayoutSubviews
 {
-    
     [super viewWillLayoutSubviews];
+    
+    self.BannerAd = [[ADBannerView alloc] initWithFrame:CGRectZero];
+    self.BannerAd.delegate = self;
+    [_BannerAd setFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.height, [[UIScreen mainScreen] bounds].size.width)]; // set to your screen dimensions
+    [self.view addSubview:_BannerAd];
     
     // Configure the view.
     SKView * skView = (SKView *)self.view;
@@ -31,6 +39,26 @@
         
         // Present the scene.
         [skView presentScene:scene];
+    }
+}
+
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    if (banner.isBannerLoaded) {
+        [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
+        // Assumes the banner view is placed at the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
+        [UIView commitAnimations];
+    }
+}
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    if (!banner.isBannerLoaded) {
+        [UIView beginAnimations:@"animateAdBannerOn" context:NULL];
+        // Assumes the banner view is just off the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, -banner.frame.size.height);
+        [UIView commitAnimations];
     }
 }
 
